004462ba7532da0b178f3b20a90cabc06fd41140
LoganMartha <logan.martha@snhu.edu>
2025-06-11 22:10:45 -0400

    Module 6 Completed Baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -42,17 +42,81 @@ const tripsFindByCode = async(req, res) => {
     if(!q) {
         // Data base returned no data
         return res
-                .status(404)
-                .json(err);
+            .status(404)
+            .json(err);
     }
     else { // Return resulting trip list
         return res
-                .status(200)
-                .json(q);
+            .status(200)
+            .json(q);
     }
 };
 
+// POST: /trips - Adds a New Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async(req, res) => {
+    const newTrip = Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    });
+
+    const q = await newTrip.save();
+
+    if(!q) {
+        // Data base returned no data
+        return res
+            .status(400)
+            .json(err);
+    }
+    else { // Return new trip
+        return res
+            .status(201)
+            .json(q);
+    }
+};
+
+// PUT: /trips/:tripCode - Adds a new Trip
+// Regardless of outcome, response must include HTML status
+// and JSON message to the requesting client
+const tripsUpdateTrip = async(req, res) => {
+    const q = await Model
+        .findOneAndUpdate(
+            { 'code' : req.params.tripCode },
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }
+        )
+        .exec();
+
+        if(!q)
+        { // Database returned no data
+            return res
+            .status(400)
+            .json(err);
+        } else { // Return resulting updated trip
+            return res
+            .status(201)
+            .json(q);
+        }
+    }; 
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,16 +2,19 @@ const express = require("express"); // Express app
 const router = express.Router(); // Router logic
 
 // This is where we import the controllers we will route
-const tripsCollector = require("../controllers/trips");
+const tripsController = require("../controllers/trips");
 
 // define route for our trips enpoint
 router
     .route('/trips')
-    .get(tripsCollector.tripsList); // GET Method routes tripList
+    .get(tripsController.tripsList) // GET Method routes tripList
+    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
+    
 
 // GET Method routes tripsFindByCode - requires parameter
 router
     .route('/trips/:tripCode')
-    .get(tripsCollector.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
